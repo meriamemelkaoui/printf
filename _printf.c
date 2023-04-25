c5a49827d594165e03e7b4bb14a9756c17f4a638
Meriame Melkaoui <meriamemelkaoui2003@gmail.com>
2023-04-25 15:22:09 -0700

    commit

--- _printf.c
+++ _printf.c
@@ -1,4 +1,66 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+int no_struct(char c, int count, va_list argu)
+{
+	int j;
+	char *s;
+
+	switch (c)/* A */
+	{
+		case 'c':/* B */
+			j = va_arg(argu, int);
+			count += _putchar(j);
+			break;
+		case 's':/* C */
+			s = va_arg(argu, char *);
+			if (!s)
+			{
+				_putchar('(');
+				_putchar('n');
+				_putchar('u');
+				_putchar('l');
+				_putchar('l');
+				_putchar(')');
+				count += 6;
+			}
+			else
+				count += _putstring(s);
+			break;
+		case 'i':/* D */
+		case 'd':
+			j = va_arg(argu, int);
+			if (!j)
+			{
+				count++;
+				_putchar('0');
+			}
+			else
+				count += print_number(j);
+			break;
+		case '%':/* E */
+			count += _putchar('%');
+			break;
+		case 'b':/* F */
+			j = va_arg(argu, int);
+			count += dec_to_binary(j);
+			break;
+		case 'r': /* G */
+			s = va_arg(argu, char *);
+			count += rev_str(s);
+			break;
+		case 'R': /* H */
+			s = va_arg(argu, char *);
+			count += rot13(s);
+			break;
+		default:/* H */
+			count += 2;
+			_putchar('%');
+			_putchar(c);
+	}
+	return (count);
+}
 
 /**
 * _printf - our own printf function
